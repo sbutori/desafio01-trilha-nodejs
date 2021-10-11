44968b8c0dbc054bcb5f27bf9c5d79b634341fde
Stefano Butori <butori@gmail.com>
2021-10-10 22:21:12 -0300

    Create "POST /users" route

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
+function checksExistsUserAccount(request, response, next) {}
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  if (users.some((u) => u.username === username)) {
+    return response.status(400).json({
+      status: 'error',
+      error: 'An account with that username already exists! Choose another one',
+    });
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+  users.push(newUser);
+
+  return response.status(201).json({
+    status: 'success',
+    message: 'User account created successfully!',
+    ...newUser,
+  });
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
